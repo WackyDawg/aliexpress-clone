131727b8f94566f7a5ad5261823c674c1ba4496d
wackydawg <juliannwadinobi098@gmail.com>
2023-10-05 11:34:45 +0100

    Added configuration file for layout variations

---
+++ src/config/config.js
@@ -0,0 +1,14 @@
+// config.js
+
+// Configuration for desktop view
+exports.desktopConfig = {
+    layoutVariation: 1, // Choose 1 or 2 for desktop layout variations
+    serverPort: 3000,   // Port for the desktop server
+  };
+  
+  // Configuration for mobile view
+  exports.mobileConfig = {
+    layoutVariation: 2, // Choose 1 or 2 for mobile layout variations
+    serverPort: 4000,   // Port for the mobile server
+  };
+  
